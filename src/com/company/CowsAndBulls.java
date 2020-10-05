24848bf7564e7b08cbf5e57f5eb96d8a25c07431
Gusts Linkevics <gusts@gst.lv>
2020-10-05 21:40:09 +0300

    Class code

--- src/com/company/CowsAndBulls.java
+++ src/com/company/CowsAndBulls.java
@@ -1,7 +1,11 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CowsAndBulls {
-    private int numberToGuess;
+    private Integer numberToGuess;
+    private List<CowsAndBullsResult> results = new ArrayList<>();
 
     public CowsAndBulls() {
         restart();
@@ -9,9 +13,35 @@ public class CowsAndBulls {
 
     public void restart() {
         generateNumber();
+        results.clear();
+    }
+
+    public CowsAndBullsResult guess(Integer number) {
+        int bulls = 0;
+        int cows = 0;
+
+        var numberArray = number.toString().split("");
+        var numberToGuessArray = numberToGuess.toString().split("");
+
+        for (int i = 0; i < numberArray.length; i++) {
+            if(numberToGuessArray[i].equals(numberArray[i])) {
+                ++bulls;
+                continue;
+            }
+
+
+        }
+
+        var result = new CowsAndBullsResult(number, bulls, cows);
+        return result;
     }
 
     private void generateNumber() {
+        numberToGuess = getRandomNumber(1000, 9999);
+        System.out.println(numberToGuess);
+    }
 
+    private int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        var game = new CowsAndBulls();
+
+        game.restart();
+        game.restart();
+        game.restart();
+        game.restart();
     }
 }
